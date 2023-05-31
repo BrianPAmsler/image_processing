720e985d9a9704de844a0480c3ac95354c610bc7
Brian Amsler <brianpamsler@gmail.com>
2023-05-31 13:44:20 -0700

    edge detection done

--- src/image_filter/mod.rs
+++ src/image_filter/mod.rs
@@ -1,6 +1,6 @@
 mod filter_matrix;
 pub use filter_matrix::FilterMatrix;
-use image::{ImageBuffer, Pixel, GenericImage, Primitive};
+use image::{ImageBuffer, Pixel, GenericImage, Primitive, Rgba};
 
 fn get_pixel<SP: Primitive, P: Pixel<Subpixel = SP>, I: GenericImage<Pixel = P>>(x: i32, y: i32, img: &I) -> Option<P> {
     if x < 0 || y < 0 || x >= img.width() as i32 || y >= img.height() as i32 {
@@ -16,7 +16,7 @@ fn filter_pixel<SP: Primitive, P: Pixel<Subpixel = SP>, I: GenericImage<Pixel = 
     let mut zero = pixel.0.clone();
     zero.apply(|_| SP::DEFAULT_MIN_VALUE);
 
-    let mut sum = zero.clone();
+    let mut sum = vec![0.0f32; P::CHANNEL_COUNT as usize];
 
     let range = filter.size()  as i32 / 2;
     for x in -range..=range {
@@ -26,19 +26,22 @@ fn filter_pixel<SP: Primitive, P: Pixel<Subpixel = SP>, I: GenericImage<Pixel = 
                 None => zero.clone()
             };
 
-            sum.apply2(&px, |sp, o| {
-                let t = o.to_u8().unwrap() as f32 * filter.get((x + range) as usize, (y + range) as usize);
-                
-                sp + SP::from(t as u32).unwrap()
-            });
+            for (i, sp) in px.channels().iter().enumerate() {
+                let t = sp.to_u8().unwrap() as f32 * filter.get((x + range) as usize, (y + range) as usize);
+            
+                sum[i] += t;
+            }
         }
     }
 
-
-    sum.apply_with_alpha(|sp| {
+    let mut out = zero.clone();
+    for (i, sp) in out.channels_mut().iter_mut().enumerate() {
+        *sp = SP::from((sum[i] as i32).abs().clamp(0, 255) as u8).unwrap();
+    }
+    out.apply_with_alpha(|sp| {
         sp
     }, |_| a);
-    sum
+    out
 }
 
 pub fn filter_image<P: Pixel, I: GenericImage<Pixel = P>>(img: &I, filter: FilterMatrix) -> ImageBuffer<P, Vec<<P as Pixel>::Subpixel>> {
@@ -49,6 +52,54 @@ pub fn filter_image<P: Pixel, I: GenericImage<Pixel = P>>(img: &I, filter: Filte
             let px = img.get_pixel(x, y);
 
             out.put_pixel(x, y, filter_pixel((px, x, y), img, &filter));
+        }
+    }
+
+    out
+}
+
+pub fn combine_images<SP: Primitive, P: Pixel<Subpixel = SP>, I: GenericImage<Pixel = P>>(img1: &I, img2: &I) -> ImageBuffer<P, Vec<<P as Pixel>::Subpixel>> {
+    let mut out = ImageBuffer::new(img1.width(), img1.height());
+    
+    for x in 0..img1.width() {
+        for y in 0..img1.height() {
+            let px1 = img1.get_pixel(x, y);
+            let px2 = img2.get_pixel(x, y);
+
+            let mut px_out = px1.clone();
+            px_out.apply2(&px2, |sp, o| {
+                SP::from((sp.to_f32().unwrap() + o.to_f32().unwrap()) / 2.0).unwrap()
+            });
+
+            out.put_pixel(x, y, px_out);
+        }
+    }
+
+    out
+}
+
+pub fn combine_color_channels<SP: Primitive, P: Pixel<Subpixel = SP>, I: GenericImage<Pixel = P>>(img: &I) -> ImageBuffer<P, Vec<<P as Pixel>::Subpixel>> {
+    let mut out = ImageBuffer::new(img.width(), img.height());
+    
+    for x in 0..img.width() {
+        for y in 0..img.height() {
+            let mut px = img.get_pixel(x, y);
+
+            let mut sum = 0;
+            let mut count = 0;
+
+            px.apply_without_alpha(|sp| {
+                sum += sp.to_u8().unwrap() as u32;
+                count += 1;
+
+                sp
+            });
+
+            px.apply_without_alpha(|_| {
+                SP::from(sum / count).unwrap()
+            });
+
+            out.put_pixel(x, y, px);
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,37 @@ mod image_filter;
 use image::io::Reader as ImageReader;
 use image_filter::{filter_image, FilterMatrix};
 
-const MATRIX: [[f32; 3]; 3] = [[0.0, 0.0, 0.0],
-                               [0.0, 1.0, 0.0],
-                               [0.0, 0.0, 0.0]];
+const GRADIENT_H: [[f32; 3]; 3] = [[-1.0, 0.0, 1.0],
+                               [-2.0, 0.0, 2.0],
+                               [-1.0, 0.0, 1.0]];
 
+const GRADIENT_V: [[f32; 3]; 3] = [[1.0, 2.0, 1.0],
+                               [0.0, 0.0, 0.0],
+                               [-1.0, -2.0, -1.0]];
+
+
+const GAUSSIAN: [[f32; 9]; 9] =   [[0.0000, 0.0000, 0.0000, 0.0001, 0.0001, 0.0001, 0.0000, 0.0000, 0.0000],
+                               [0.0000,	0.0000,	0.0004,	0.0014,	0.0023,	0.0014,	0.0004,	0.0000,	0.0000],
+                               [0.0000,	0.0004,	0.0037,	0.0146,	0.0232,	0.0146,	0.0037,	0.0004,	0.0000],
+                               [0.0001,	0.0014,	0.0146,	0.0584,	0.0926,	0.0584,	0.0146,	0.0014,	0.0001],
+                               [0.0001,	0.0023,	0.0232,	0.0926,	0.1466,	0.0926,	0.0232,	0.0023,	0.0001],
+                               [0.0001,	0.0014,	0.0146,	0.0584,	0.0926,	0.0584,	0.0146,	0.0014,	0.0001],
+                               [0.0000,	0.0004,	0.0037,	0.0146,	0.0232,	0.0146,	0.0037,	0.0004,	0.0000],
+                               [0.0000,	0.0000,	0.0004,	0.0014,	0.0023,	0.0014,	0.0004,	0.0000,	0.0000],
+                               [0.0000,	0.0000,	0.0000,	0.0001,	0.0001,	0.0001,	0.0000,	0.0000,	0.0000]];
 fn main() {
     let input = ImageReader::open("input.png").unwrap().decode().unwrap();
 
-    let filter = FilterMatrix::new(MATRIX);
+    let blurred = filter_image(&input, FilterMatrix::new(GAUSSIAN));
+
     println!("Filtering...");
-    let output = filter_image(&input, filter);
+    let h = filter_image(&blurred, FilterMatrix::new(GRADIENT_H));
+    let v = filter_image(&blurred, FilterMatrix::new(GRADIENT_V));
+
+    let combined = image_filter::combine_images(&h, &v);
+    let mono = image_filter::combine_color_channels(&combined);
+
+    let output = mono;
     println!("Done.");
     output.save("output.png").unwrap();
 }
